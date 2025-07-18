76b170006f5d3e95ba4803c6ceeab1b571e7e44d
zehraoztrkk <zehraoztrk00@gmail.com>
2025-07-18 09:24:23 +0300

    feat:add responsive footer with 3 sections (About Us, Links, Contact Info)

---
+++ public/scripts/footer.js
@@ -0,0 +1,4 @@
+// Footer'da dinamik yıl göstermek istersen:
+const footerText = document.querySelector('.footer-bottom p');
+const year = new Date().getFullYear();
+footerText.innerHTML = `&copy; ${year} Tüm hakları saklıdır | Zehra Öztürk`;
